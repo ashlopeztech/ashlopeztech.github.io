fc76f11e58296f2f4cde016da3aacfe203c58616
errarrgh <ashlopeztech@gmail.com>
2017-11-10 09:32:44 -0800

    removed mm eq command

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (annyang) {
   // Let's define a command.
   var commands = {
     'hello': function() { alert('HEY GURL HE-EY!'); }
-    'mirror mirror on the wall': function() { alert('taste my forbiddin fruit'); }
+    //'mirror mirror on the wall': function() { alert('taste my forbiddin fruit'); }
   };
   // Add our commands to annyang
   annyang.addCommands(commands);
