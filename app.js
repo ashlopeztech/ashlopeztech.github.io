00c5f05c25a43cb9d31c37dce5c854b14f20762a
errarrgh <ashlopeztech@gmail.com>
2017-11-09 20:13:20 -0800

    added mirror mirror command

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-
-   if (annyang) {
-     // Let's define a command.
-      var commands = {
-    'hello': function() { 
+if (annyang) {
+  // Let's define a command.
+  var commands = {
+    'hello': function(){ 
       alert('Hello world!'); 
     }
+    'mirror mirror on the wall who is the greatest queen of all?': function () {
+      alert("Well me of course!");
+      
+    }
+
   };
 
   // Add our commands to annyang
